a464d0a68156291a4945a060e93125bf0896bc6b
Julien GUTTER <julien.gutter@gmail.com>
2024-03-17 20:07:27 +0100

    Correction du code pour comptage du nombre d'éléments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,6 +3,8 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
@@ -11,12 +13,43 @@ public class AnalyticsCounter {
 	
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader (new FileReader("/Users/juliengutter/git/GUTTER_Julien_Debug_JAVA/Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine();
 
 		int i = 0;	// set i to 0
 		int headCount = 0;	// counts headaches
+		
+		TreeMap<String,Integer> map=new TreeMap<String,Integer>(); 
+		
 		while (line != null) {
+			
+			
+			
+			// Correction du code
+			System.out.println("symptom from file: " + line);
+			  
+			
+			if(map.containsKey(line)) {
+				int j;
+				j=map.get(line);
+				j++;
+				map.replace(line, j);
+				System.out.println("Déja présent ");
+				
+			}
+			else {
+				
+				map.put(line, 1);
+				System.out.println("Nouveau");
+			}
+		
+			
+	  
+		        
+		         
+		      
+		      
+		      
 			i++;	// increment i
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
@@ -39,5 +72,12 @@ public class AnalyticsCounter {
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
+		
+		
+		System.out.println("Résultat final:"); 
+		for(Map.Entry m:map.entrySet()){    
+			System.out.println(m.getKey()+" "+m.getValue());    
+		} 
+		
 	}
 }
